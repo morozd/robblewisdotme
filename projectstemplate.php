8e2ccc453c2fce73c9f1bdfcdc2c26f840a80306
Robb Lewis <me@therobb.com>
2013-03-11 15:42:55 +0000

    Updated projects page

--- projectstemplate.php
+++ projectstemplate.php
@@ -1,4 +1,4 @@
-<?php /*Template Name: Projects*/?>
+<?php /*Template Name: Projects */ ?>
 
     <?php get_header();?>
 
@@ -11,35 +11,59 @@
 
          		<h2><?php the_title();?></h2>
 
-                <h3>GitHub Repositories</h3>
+                <p><?php the_content('Read more &raquo;'); ?></p>
+
+                <!--Start of child pages-->
+
+                <?php
+                    // Set up the arguments for retrieving the pages
+                    $args = array(
+                        'post_type' => 'page',
+                        'numberposts' => -1,
+                        'post_status' => null,
+                    // $post->ID gets the ID of the current page
+                    'post_parent' => $post->ID,
+                        'order' => ASC,
+                        'orderby' => menu_order
+                        );
+                     $subpages = get_posts($args);
+                     // Just another WordPress Loop
+                     foreach($subpages as $post) :
+                        setup_postdata($post);
+                     ?>
+
+                    <article class="project" id="<?php the_title();?>">
+
+                    <h3><a href="<?php the_permalink(); ?>"id="post-<?php the_ID(); ?>"><?php the_title(); ?> &raquo;</a></h3>
+                    <p><?php the_excerpt(); ?></p>
+                   </article>
+                    
+                  <?php endforeach; ?>
+
+                  <!--End of child pages-->
+
+            </article>
+    
+    <?php endwhile; ?> 
+
+    <article class="project">
+    <h3><a href="https://github.com/rmlewisuk?tab=repositories">GitHub Repositories &raquo;</a></h3>
 
                 <!-- Javascript to load and display repos from GitHub -->
                 <script src="http://ajax.microsoft.com/ajax/jquery/jquery-1.4.2.min.js" type="text/javascript"></script>
                 <script src="<?php bloginfo('template_directory'); ?>/scripts/gitfull.js" type="text/javascript"></script>
                 <script type="text/javascript">
-                $(function() {
-                  $("#gitrepos").loadRepositories("rmlewisuk");
-                });
+                    $(function() {
+                      $("#gitrepos").loadRepositories("rmlewisuk");
+                    });
                 </script>
-            <!-- End GitHub repo code -->
+                <!-- End GitHub repo code -->
 
             <ul>
             <span id="gitrepos"></span>
             </ul>
 
-            <h3>Miscellaneous</h3>
-            <ul>
-              <?php
-              global $id;
-              wp_list_pages("title_li=&child_of=$id&show_date=modified
-              &date_format=$date_format"); ?>
-            </ul>
-
-                <p><?php the_content('Read more &raquo;'); ?></p>
-
-            </article>
-    
-    <?php endwhile; ?> 
+          </article>
 
     </section>
     
