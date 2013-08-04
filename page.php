d8b7356f03c2eee5bc07ef8cb8bc3673cb5bf991
Robb Lewis <me@therobb.com>
2013-08-04 16:36:50 +0100

    Updated single page template

--- page.php
+++ page.php
@@ -7,38 +7,15 @@
 	<?php if (have_posts()) : ?>  
     <?php while (have_posts()) : the_post(); ?>
 
-         		<h2><?php the_title();?></h2>
+            <article class="basket-whole main">
 
-                <p><a href="#" class="focus">Focus</a></p>
-
-        <script>
-    $(".focus").click(function () {
-      if($(this).text() == 'Focus')
-       {
-          $("header").slideToggle("slow");
-        $("nav").slideToggle("slow");
-        $(".meta").slideToggle("slow");
-        $("footer").slideToggle("slow");
-          $("img").animate({"opacity":"0.3"}, 550);
-          $(this).text('Unfocus');
-       }
-       else
-       {
-          $("header").slideToggle("slow");
-      $("nav").slideToggle("slow");
-      $(".meta").slideToggle("slow");
-      $("footer").slideToggle("slow");
-          $("img").animate({"opacity":"1"}, 550);
-          $(this).text('Focus');
-       }
-    });
-</script>
-
+                <h2 class="title"><?php the_title();?></h2>
+    
                 <p><?php the_content('Read more &raquo;'); ?></p>
 
             </article>
-    
-    <?php endwhile; ?> 
+
+    <?php endwhile; ?>
 
     </section>
     
